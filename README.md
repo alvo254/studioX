c7e7de2287f088041ca4377fcf124f7a0cd4a30e
Alvin <alvinkhamron@gmail.com>
2024-02-22 14:13:24 +0300

    [vpc creation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # studioX
+
 EFS file share
---
+++ modules/vpc/main.tf
@@ -0,0 +1,56 @@
+locals {
+  network = yamldecode(file("${path.module}/network.yaml"))
+}
+
+resource "aws_vpc" "studioX-vpc"{
+    instance_tenancy = "default"
+    cidr_block = local.network.cidr_block
+    enable_dns_hostnames = true
+    assign_generated_ipv6_cidr_block = true
+    tags = {
+      Name = "studioX"
+    }
+    
+    
+}
+data "aws_availability_zones" "available_zones" {}
+
+
+resource "aws_subnet" "studioX-pub-sub1" {
+    vpc_id                  = aws_vpc.studioX-vpc.id
+    availability_zone       = data.aws_availability_zones.available_zones.names[0]
+    map_public_ip_on_launch = true
+    assign_ipv6_address_on_creation = true 
+    cidr_block = var.cird
+
+    tags = {
+      Name = "studioX-subnets"
+    }
+    
+    //This lets aws pick from IPAM pool
+    ipv6_cidr_block         = "${cidrsubnet(aws_vpc.studioX-vpc.ipv6_cidr_block, 8, 1)}"
+
+    //This is something I am experimenting on
+    # for_each = { for s in local.network.subnets : s.name => s }
+    # ipv6_cidr_block         = each.value.ipv6_cidr_block
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.studioX-vpc.id
+  
+}
+
+resource "aws_route_table" "studioX-rtb" {
+  vpc_id = aws_vpc.studioX-vpc.id
+ 
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "pub_sub1_association" {
+  # for_each        = aws_subnet.studioX-pub-sub1
+  subnet_id       = aws_subnet.studioX-pub-sub1.id
+  route_table_id  = aws_route_table.studioX-rtb.id
+}
